ca0c326c6a1fd3105163d945be71f14d92a3aff6
Laércio <laercio.santos04@gmail.com>
2025-04-07 22:45:53 -0300

    Node

--- app/routes/router.js
+++ app/routes/router.js
@@ -10,14 +10,25 @@ router.get("/", verificarUsuAutenticado, function (req, res){
     res.render("pages/index", req.session.autenticado);
 });
 
+router.get("/sair", limparSessao, function (req, res) {
+    res.redirect("/");
+  });
+
 router.get("/login", function (req, res){
     res.render("pages/login", { listaErros: null });
 } )
 
-router.get("/cadastro", (req, res)=>{
-    res.render("pages/cadastro", 
-        {"listaErros":null, "valores":{nome:"", senha:"","csenha":"", email:""}, "falha":null})
+router.post(
+    "/login",
+    usuarioController.regrasValidacaoFormLogin,
+    gravarUsuAutenticado,
+    function (req, res) {
+      usuarioController.logar(req, res);
 })
+
+router.get("/cadastro", function (req, res) {
+    res.render("pages/cadastro", { listaErros: null, valores: { nome_usu: "", nomeusu_usu: "", email_usu: "", senha_usu: "" } });
+  });
 
 router.post("/cadastro", 
     usuarioController.validacaoFormularioCadastro, 
@@ -41,13 +52,7 @@ router.get("/teste", async (req, res) =>{
     //let resultados = await usuarioModel.findAll();
 })
 
-router.post(
-    "/login",
-    usuarioController.regrasValidacaoFormLogin,
-    gravarUsuAutenticado,
-    function (req, res) {
-      usuarioController.logar(req, res);
-})
+
   
 
 router.post("/cadastro",
